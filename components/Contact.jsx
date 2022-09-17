80b451f95efc457a206a6e384552fc7a24850b71
LucaPalminteri <lucapalminteri02@gmail.com>
2022-09-17 10:11:22 -0300

    add contact links

---
+++ components/Contact.jsx
@@ -0,0 +1,17 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LanguageIcon from '@mui/icons-material/Language';
+
+function Contact() {
+
+    return (
+        <div className="contact">
+            <h3>Luca Palminteri</h3>
+            <a href="https://lucapalminteri.com/"><LanguageIcon/></a>
+            <a href="https://www.linkedin.com/in/luca-palminteri/"><LinkedInIcon/></a>
+            <a href="https://github.com/LucaPalminteri"><GitHubIcon/></a>
+        </div>
+    )
+  }
+  
+  export default Contact
