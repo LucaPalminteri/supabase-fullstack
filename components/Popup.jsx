c7179b5cc337de932b7be4210829ac57c0cb7aa0
LucaPalminteri <lucapalminteri02@gmail.com>
2022-09-17 09:16:39 -0300

    add cancel update option

--- components/Popup.jsx
+++ components/Popup.jsx
@@ -3,6 +3,10 @@ function Popup() {
     return (
         <div className="popup">
             Popup
+            <div className="btn-container">
+                <button>cancel</button>
+                <button>delete</button>
+            </div>
         </div>
     )
   }
