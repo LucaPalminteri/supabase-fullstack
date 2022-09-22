50ba176cd34a830d504499d6c4e2784951bc3876
LucaPalminteri <lucapalminteri02@gmail.com>
2022-09-22 08:40:11 -0300

    feat: change local variables

--- utils/client.js
+++ utils/client.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL, 
-    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_URL || SUPABASE_URL, 
+    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY || SUPABASE_SERVICE_ROLE_KEY
 );
 
-export {supabase}
+export { supabase }
 
--- utils/env-variables.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV != 'production') {
-    require('dotenv').config()
-}
-
-process.env.PUBLIC_ANON_KEY='fawefawe';
